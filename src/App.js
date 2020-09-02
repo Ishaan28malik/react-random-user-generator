344fde79d4010bb0eaffec204e2285f93c7ed2f1
Ishaan <ishaanmalikrm7@gmail.com>
2020-09-02 20:04:34 +0530

    styling

--- src/App.js
+++ src/App.js
@@ -6,28 +6,71 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pictures: []
+      pictures: [],
     };
   }
 
   componentDidMount() {
-    setInterval(() => this.repeat(), 1000);
+    setInterval(() => this.repeat(), 2000);
   }
 
   repeat() {
     fetch("https://randomuser.me/api/?results=1")
-      .then(results => {
+      .then((results) => {
         return results.json();
       })
-      .then(data => {
-        let pictures = data.results.map(person => {
+      .then((data) => {
+        let pictures = data.results.map((person) => {
           return (
             <div>
               <div key={person.results}>
-                <img src={person.picture.large} />
-                <h1>First Name: {person.name.first}</h1>
-                <h1>Gender: {person.gender}</h1>
-                <h1>Age: {person.dob.age}</h1>
+                <img
+                  src={person.picture.large}
+                  style={{
+                    marginTop: "-130px",
+                    marginLeft: "85px",
+                    height: "200px",
+                    width: "200px",
+                    borderRadius: "100px",
+                    border: "5px solid blue",
+                  }}
+                />
+                <h1
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    color: "white",
+                  }}
+                >
+                  First Name: {person.name.first}
+                </h1>
+                <h1
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    color: "white",
+                  }}
+                >
+                  Gender: {person.gender}
+                </h1>
+                <h1
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    color: "white",
+                  }}
+                >
+                  Age: {person.dob.age}
+                </h1>
+                <h1
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    color: "white",
+                  }}
+                >
+                  Country:{person.location.country}
+                </h1>
               </div>
             </div>
           );
@@ -38,9 +81,23 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="container1">
-        <div className="container2">{this.state.pictures}</div>
-      </div>
+      <>
+        {/* <h1>Hi there</h1> */}
+        <div
+          style={{
+            // border: "2px solid",
+            borderRadius: "20px",
+            height: "520px",
+            width: "450px",
+            marginTop: "150px",
+            backgroundColor: "blue",
+          }}
+        >
+          <div className="container1" style={{ margin: "30px" }}>
+            <div className="container2">{this.state.pictures}</div>
+          </div>
+        </div>
+      </>
     );
   }
 }
