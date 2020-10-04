17884f4b5850b269c136fab6cd2968b86858a3fc
Satyam1203 <nakulbegana191170@gmail.com>
2020-10-04 20:06:21 +0530

    Formatted the view and added DOB & phone

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import React from "react";
+import Loader from "./components/Loader";
+
+const style = {
+  display: "grid",
+  gridTemplateColumns: "0.8fr 1fr",
+  justifyItems: "left",
+  color: "white",
+};
 
 class App extends React.Component {
   constructor(props) {
@@ -24,65 +32,40 @@ class App extends React.Component {
       .then((data) => {
         let pictures = data.results.map((person) => {
           return (
-            <div>
-              <div key={person.results}>
+              <div key={person.login.uuid} style={{textAlign: 'center'}}>
                 <img
                   src={person.picture.large}
-                  className='profile-pic'
+                  className="profile-pic"
                   style={{
                     marginTop: "-130px",
-                    marginLeft: "85px",
                     height: "200px",
                     width: "200px",
                     borderRadius: "100px",
                     border: "5px solid #ff337d",
                   }}
-                  alt='profile-img'
+                  alt="profile-img"
                 />
-                <h1
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    color: "white",
-                  }}
-                >
-                  First Name: {person.name.first}
-                </h1>
-                <h1
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    color: "white",
-                  }}
-                >
-                  Gender: {person.gender}
-                </h1>
-                <h1
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    color: "white",
-                  }}
-                >
-                  Age: {person.dob.age}
-                </h1>
-                <h1
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    color: "white",
-                  }}
-                >
-                  Country: {person.location.country}
-                </h1>
-                <button className='random-btn' onClick={() => this.repeat()}>
+                <div style={style}>
+                  <h2>First Name </h2>
+                  <h2>: {person.name.first}</h2>
+                  <h2>Gender</h2>
+                  <h2>: {person.gender}</h2>
+                  <h2>Age</h2>
+                  <h2>: {person.dob.age}</h2>
+                  <h2>Country</h2>
+                  <h2>: {person.location.country}</h2>
+                  <h2>Phone</h2>
+                  <h2>: {person.phone}</h2>
+                  <h2>DOB</h2>
+                  <h2>: {person.dob.date.slice(0, 10)}</h2>
+                </div>
+                <button className="random-btn" onClick={() => this.repeat()}>
                   New random user!
                 </button>
               </div>
-            </div>
           );
         });
-        this.setState({ pictures: pictures });
+        this.setState({ pictures });
       });
   }
 
@@ -98,53 +81,13 @@ class App extends React.Component {
             backgroundColor: "#222831",
           }}
         >
-          <div className="container1" style={{ margin: "30px" }}>
+          <div className="container1" style={{ margin: "25px" }}>
             <div className="container2">
-              {this.state.loading
-                ? this.loadingAnimation()
-                : this.state.pictures}
+              {this.state.loading ? <Loader /> : this.state.pictures}
             </div>
           </div>
         </div>
       </>
-    );
-  }
-  loadingAnimation() {
-    return (
-      <div>
-        <svg
-          aria-hidden="true"
-          focusable="false"
-          data-prefix="far"
-          data-icon="spinner-third"
-          class="svg-inline--fa fa-spinner-third fa-w-16"
-          role="img"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 512 512"
-        >
-          <path
-            fill="currentColor"
-            d="M460.116 373.846l-20.823-12.022c-5.541-3.199-7.54-10.159-4.663-15.874 30.137-59.886 28.343-131.652-5.386-189.946-33.641-58.394-94.896-95.833-161.827-99.676C261.028 55.961 256 50.751 256 44.352V20.309c0-6.904 5.808-12.337 12.703-11.982 83.556 4.306 160.163 50.864 202.11 123.677 42.063 72.696 44.079 162.316 6.031 236.832-3.14 6.148-10.75 8.461-16.728 5.01z"
-          ></path>
-        </svg>
-
-        <svg
-          id="shadow"
-          aria-hidden="true"
-          focusable="false"
-          data-prefix="far"
-          data-icon="spinner-third"
-          class="svg-inline--fa fa-spinner-third fa-w-16"
-          role="img"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 512 512"
-        >
-          <path
-            fill="currentColor"
-            d="M460.116 373.846l-20.823-12.022c-5.541-3.199-7.54-10.159-4.663-15.874 30.137-59.886 28.343-131.652-5.386-189.946-33.641-58.394-94.896-95.833-161.827-99.676C261.028 55.961 256 50.751 256 44.352V20.309c0-6.904 5.808-12.337 12.703-11.982 83.556 4.306 160.163 50.864 202.11 123.677 42.063 72.696 44.079 162.316 6.031 236.832-3.14 6.148-10.75 8.461-16.728 5.01z"
-          ></path>
-        </svg>
-      </div>
     );
   }
 }
